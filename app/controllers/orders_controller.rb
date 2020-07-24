5d824d8f9ccab9758786edd5bb03e227bf307826
Matthew Gaudio <mattgaud2425@gmail.com>
2020-07-24 17:44:22 -0400

    progress ch.12

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:new, :create]
+  before_action :ensure_cart_isnt_empty, only: :new
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+
 
   # GET /orders
   # GET /orders.json
@@ -70,5 +74,11 @@ class OrdersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
+    end
+
+    def ensure_cart_isnt_empty
+      if @cart.line_items.empty?
+        redirect_to store_index_url, notice: 'your cart is empty'
+      end
     end
 end
